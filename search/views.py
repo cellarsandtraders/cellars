83a44103a0ad9712df7ecc06213edaa4a1b91416
Adam Wonak <adam@getfoodgenius.com>
2015-01-25 15:06:01 -0600

    Handle no connection to BreweryDB

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.conf import settings
 from utils import json_response, endpoint
 from utils.brewerydb import BreweryDb
@@ -11,5 +12,14 @@ BreweryDb.configure(
 @endpoint
 def search(request):
     if request.method == 'GET' and request.GET.get('q'):
-        results = BreweryDb.search({'type': 'beer', 'q': request.GET['q']})
-        return json_response(results)
+        try:
+            results = BreweryDb.search({
+                'type': 'beer',
+                'withBreweries': 'Y',
+                'q': request.GET['q']
+            })
+            return json_response(results)
+        except requests.ConnectionError:
+            return json_response({
+                'error': 'Cannot connect to BreweryDb'
+            }, status=503)
